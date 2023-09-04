31b209510d703e618c1befc597ad34c30c58b2af
Sara Veldhoen <s.veldhoen@beeldengeluid.nl>
2023-09-04 13:18:11 +0200

    stripped audio extractor to bare functionalities, because transforming timestamps is unneccessary

--- AudioExtractorUtil.py
+++ AudioExtractorUtil.py
@@ -1,79 +1,29 @@
 import logging
 from datetime import datetime, time, timedelta
-from FfMpegUtil import FfMpegUtil
+import ffmpeg
+import os
 
-logging.basicConfig(level=logging.DEBUG, format="%(message)s")
-
-logger = logging.getLogger("audio_extractor_util")
-
-
-class AudioExtractorUtil(FfMpegUtil):
-    """Utility class to provide functions that extract audio fragments
-    from input media file. This class is created specifically for VisXP.
-    """
-
-    def __init__(self):
-        super(FfMpegUtil, self).__init__()
-
-    def extract_audio_fragments_from_list(self, timecodes: list) -> None:
-        """Read a list of timecodes and extract audio fragments for each of them."""
-        if isinstance(timecodes, list) is False:
-            logging.info("A list of timecodes is needed.")
-        for tc in timecodes:
-            try:
-                ts = self.generate_timestamps_visxp(tc)
-                self.extract_audio_fragment(start_time=ts.get("begin"), end_time=ts.get("end"))
-            except ValueError as e:
-                logger.error(e)
-
-    def generate_timestamps_visxp(self, timecode: str, delta_secs: float = 0.5) -> dict:
-        """Given a timecode, generate the start- and end timestamp for the audio fragment.
-        :param timecode: a timestamp
-        :param delta_secs: a float indicating the time (in seconds) to be extracted
-            before and after the timecode.
-        """
-        # TODO: find out what the real timestamps are that we can expect.
-        if self.is_valid_timestamp(timecode):
-            for fmt in ("%H:%M:%S.%f", "%H:%M:%S"):
-                try:
-                    tm = datetime.strptime(timecode, fmt).time()
-                    if isinstance(tm, time):
-                        return {
-                            "begin": (
-                                datetime.combine(datetime.now(), tm) - timedelta(seconds=delta_secs)
-                            )
-                            .time()
-                            .isoformat(),
-                            "end": (
-                                datetime.combine(datetime.now(), tm) + timedelta(seconds=delta_secs)
-                            )
-                            .time()
-                            .isoformat(),
-                        }
-                except ValueError:
-                    pass
-        raise ValueError(f"Not a valid time code: {timecode}")
-
-    def run(self, timecodes: list) -> None:
-        """Describe the things you want to get done and run!"""
-        for timestamp in timecodes:
-            ts = self.generate_timestamps_visxp(timestamp)
-            self.extract_audio_fragment(start_time=ts.get("begin"), end_time=ts.get("end"))
-        print("Done!")
+logger = logging.getLogger(__name__)
 
 
-if __name__ == "__main__":
-    media_file = "/mnt/c/Users/wmelder/Downloads/WEEKNUMMER374-HRE0000D0ED_1748000_1782000.mp4"
-    worker = AudioExtractorUtil()
-    list_of_timecodes = [
-        "00:00:04",
-        "00:00:08.500",
-        "00:00:18.123456",
-        "00:00:33.500",
-    ]
-    worker.set_media_source_file(filepath=None)
-    worker.run(list_of_timecodes)  # no media file set
-    worker.set_media_source_file(filepath=media_file)
-    worker.run(list_of_timecodes)  # smooth as vanilla ice cream
-    list_of_timecodes = ["00:er:ro.r"]
-    worker.run(list_of_timecodes)  # value error expected
+
+def extract_audio_fragments(media_file: str, keyframe_timestamps: dict[int:int], location: str, window_size : int = 1000):
+    if not os.path.exists(media_file):
+        raise IOError('Input video not found')
+
+    logger.info(f"Extracting audio from {media_file} to {location}.")
+
+    for i, (keyframe_index, timestamp) in enumerate(keyframe_timestamps.items()):
+            
+            
+        #try:
+            out_file = os.path.join(location, f"{keyframe_index}.wav")
+            if i % 100 == 0: print(f'Keyframe index: {keyframe_index} at time {timestamp} ms. Extracting audio to {out_file}.')
+            # TODO: group together as one ffmpeg command rather than this for-loop
+            ffmpeg.input(media_file).output(
+                            filename = out_file,
+                            **{"map": "0:a", "ss": f"{int(timestamp-0.5*window_size)}ms", "to":  f"{int(timestamp+0.5*window_size)}ms"}, # TODO: edge case if start/end time not in video (e.g. negative start)
+                        ).run(quiet=True, overwrite_output=True) # set quiet to False for debugging
+            if i>200: break
+        #except ffmpeg.Error as e:
+        #    logger.exception(e)
