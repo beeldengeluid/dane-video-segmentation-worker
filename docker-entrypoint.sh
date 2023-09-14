334d115b73843654fc0d7bba8b0ab5cb2efa2153
Jaap Blom <jblom@beeldengeluid.nl>
2023-09-14 11:52:27 +0200

    adding tail debug trick + made docker-entrypoint.sh executable

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-echo "Starting virtual env and DANE video segmentation worker"
+# echo "Starting virtual env and DANE video segmentation worker"
 
-poetry run python worker.py
+# poetry run python worker.py
+
+echo the worker crashed, tailing /dev/null for debugging
+
+tail -f /dev/null
