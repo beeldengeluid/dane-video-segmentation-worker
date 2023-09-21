9a127864290a5cc1f749ac78acab325052c44d1c
Sara Veldhoen <s.veldhoen@beeldengeluid.nl>
2023-09-21 11:22:18 +0200

    Return filenames of keyframe files from keyframe_util

--- keyframe_util.py
+++ keyframe_util.py
@@ -3,24 +3,23 @@ import os
 
 import logging
 
-from typing import Dict
 
 logger = logging.getLogger(__name__)
 
 
 def extract_keyframes(
     media_file: str, keyframe_indices: list[int], out_dir: str
-) -> dict[int, int]:
+) -> list[str]:
     if not os.path.exists(media_file):
         raise IOError("Input video not found")
 
-    keyframe_timestamps: Dict[int, int] = {}
     vcap = cv2.VideoCapture(media_file)
     if not vcap.isOpened():
         raise IOError("Unable to open video file " + media_file)
 
     next_i = int(vcap.get(cv2.CAP_PROP_POS_FRAMES))
     max_i = max(keyframe_indices)
+    fns = []
     while next_i <= max_i:
         ret = vcap.grab()
         if next_i in keyframe_indices:
@@ -29,8 +28,9 @@ def extract_keyframes(
                 _, frame = vcap.retrieve()
                 fn = os.path.join(out_dir, f"{timestamp}.jpg")
                 cv2.imwrite(fn, frame)
+                fns.append(fn)
             else:
                 raise IOError(f"Unable to read keyframe {next_i}")
         next_i = int(vcap.get(cv2.CAP_PROP_POS_FRAMES))
     vcap.release()
-    return keyframe_timestamps
+    return fns
