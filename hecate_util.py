85006b806e89ef9795d7139dfefb71c57af51144
Sara Veldhoen <s.veldhoen@beeldengeluid.nl>
2023-09-21 11:21:38 +0200

    Output of Hecate to file moved to hecate_util

--- hecate_util.py
+++ hecate_util.py
@@ -1,5 +1,6 @@
 import base_util
 import logging
+import os
 
 logger = logging.getLogger(__name__)
 hecate_path = "/hecate/distribute/bin/hecate"
@@ -40,3 +41,34 @@ def interpret_hecate_output(
                 for keyframe_index in line[len("keyframes: ") :][1:-1].split(",")
             ]
     return (shots, keyframes)
+
+
+def write_to_file(shots, keyframes, metadata_dir, fps):
+    shots_times_path = os.path.join(metadata_dir, "shot_boundaries_timestamps_ms.txt")
+    keyframe_indices_path = os.path.join(metadata_dir, "keyframe_indices.txt")
+    keyframe_times_path = os.path.join(metadata_dir, "keyframes_timestamps_ms.txt")
+    with open(shots_times_path, "w",) as f:
+        f.write(
+            str(
+                [
+                    (
+                        _frame_index_to_timecode(start, fps),
+                        _frame_index_to_timecode(end, fps),
+                    )
+                    for (start, end) in shots
+                ]
+            )
+        )
+
+    with open(keyframe_indices_path, "w") as f:
+        f.write(str(keyframes))
+    with open(keyframe_times_path, "w") as f:
+        f.write(str([_frame_index_to_timecode(i, fps) for i in keyframes]))
+    return {'shot_boundaires': shots_times_path,
+            'keyframe_indices': keyframe_indices_path,
+            'keyframes_timestamps': keyframe_times_path}
+
+
+def _frame_index_to_timecode(frame_index: int, fps: float, out_format="ms"):
+    if out_format == "ms":
+        return round(frame_index / fps * 1000)
