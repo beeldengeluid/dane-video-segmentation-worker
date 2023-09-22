187b6bdae9e578c08d9dc3663dd78e86201abc59
Sara Veldhoen <s.veldhoen@beeldengeluid.nl>
2023-09-22 10:23:34 +0200

    Black (disable formatting for commented test input)

--- tests/unit/visxp_prep_test.py
+++ tests/unit/visxp_prep_test.py
@@ -7,9 +7,13 @@ def test_filter_edge_keyframes():
     framecount = int(duration * fps)
     edge_1 = int(fps / 2)  # 14
     edge_2 = int(framecount - fps / 2)  # 284
-    frame_indices = [-2, 0, 3, edge_1,  # too close to the start
-                     edge_1 + 1, 29, 30, 100, edge_2,  # OK
-                     edge_2 + 1, framecount, framecount + 1]  # too close to the end
+    # fmt: off
+    frame_indices = [
+        -2, 0, 3, edge_1,  # too close to the start
+        edge_1 + 1, 29, 30, 100, edge_2,  # OK
+        edge_2 + 1, framecount, framecount + 1  # too close to the end
+    ]
+    # fmt: on
     expected_result = [edge_1 + 1, 29, 30, 100, edge_2]
     result = _filter_edge_keyframes(frame_indices, fps=fps, framecount=framecount)
     assert result == expected_result
